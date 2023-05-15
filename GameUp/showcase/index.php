085bee944679add0ae367a84796c39619236e912
Alexandr0007 <alexandrklim00@gmail.com>
2023-05-15 22:37:51 +0500

    design

---
+++ GameUp/showcase/index.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div id="header"><image id="logo" src="logo.jpg"></image>
+    GameUp!
+    <button><img src="user.png" alt="аккаунт" id="icon"></button>
+    <button><img src="basket.png" alt="корзина" id="icon"></button>
+
+</div>
+<div id="content"><div id="special">Специальные предложения!</div>
+    <table id="tr1">
+        <tr>
+            <td id="sales">Блок 1</td>
+            <td id="sales">Блок 2</td>
+            <td id="sales">Блок 3</td>
+            <td id="sales">Блок 4</td>
+        </tr>
+    </table>
+    <table>
+        <tr>
+            <td>Блок 1</td>
+            <td>Блок 2</td>
+            <td>Блок 3</td>
+            <td>Блок 4</td>
+            <td>Блок 5</td>
+            <td>Блок 6</td>
+        </tr>
+        <tr>
+            <td>Блок 1</td>
+            <td>Блок 2</td>
+            <td>Блок 3</td>
+            <td>Блок 4</td>
+            <td>Блок 5</td>
+            <td>Блок 6</td>
+        </tr>
+    </table>
+
+</div>
+</body>
+</html>
