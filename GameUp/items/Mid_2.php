6d9cc1c4942e25501650245f321834e504d900ff
VsevolodBegletsov267 <119685217+VsevolodBegletsov267@users.noreply.github.com>
2023-05-31 09:39:42 +0300

    Update and rename Mid_2.html to Mid_2.php

--- GameUp/items/Mid_2.php
+++ GameUp/items/Mid_2.php
@@ -1,3 +1,7 @@
+<?php
+require_once "config.php";
+session_start();
+?>
 <!doctype html>
 <html lang="ru">
 <head>
@@ -11,7 +15,9 @@
     <image id="logo" src="../showcase/logo.jpg"></image>
     GameUp!
     <button><img src="../showcase/user.png" alt="аккаунт" id="icon"></button>
+    <form action="">
     <button><img src="../showcase/basket.png" alt="корзина" id="icon"></button>
+    </form>
     <div id="slogan">Лучшее для лучших!</div>
 </div>
 <div id="content">
@@ -20,11 +26,13 @@
 
   <img src="../showcase/Goodimage/HyperX Pulsefire Surge.jpg" alt="Картинка" id="imageproduct">
   
-     <div id="item">HyperX Pulsefire Surge</div>
+     <div id="item"><?php $name = 'HyperX Pulsefire'; ?></div>
 
-        <p id="price">Цена:2.969 р.</p>
+        <p id="price">Цена: <?php $price = 2969; ?>р.</p>
 
+        <form method="POST">
         <button class="buy-button">Купить</button>
+        </form>
 
 <style>
 .buy-button {
@@ -63,10 +71,13 @@
 Модель поддерживает настройку эффектов подсветки, программных кнопок, разрешения, но самое главное в том, что все эти настройки можно сохранять в память при помощи программного обеспечения HyperX NGenuity. Круговая RGB-подсветка поддерживает динамические эффекты благодаря световому кольцу. HyperX Pulsefire Surge RGB HX-MC002B оборудована 6 кнопками, среди которых особое место отводится программируемым клавишам, позволяющим достичь высот в игровых сражениях.</p>
   </div>
 </div>
-
-
-        
-    </div>
- 
+</div>
 </body>
 </html>
+<?php
+$mysql = new mysqli('localhost', 'root', '1029384756Seva', 'gameup');
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $mysql->query("INSERT INTO `products`(id, name, price) VALUES(NULL, '$name', $price)");
+    header("Location: http://localhost/GameUp/showcase/index1.php");
+}
+?>
