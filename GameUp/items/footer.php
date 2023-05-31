804c4559af794e93c7d47a8f96b8a285f3ced5fe
MiKerfr <119686068+MiKerfr@users.noreply.github.com>
2023-05-31 21:15:03 +0300

    FOOTER

--- GameUp/items/footer.php
+++ GameUp/items/footer.php
@@ -1,18 +1,8 @@
 </div>
 <footer>
   <div class="container">
-    <div class="row">
-      <div class="col-md-6">
-        <p>Контактная информация</p>
-      </div>
-      <div class="col-md-6">
-        <ul class="social">
-          <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-          <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-          <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-        </ul>
-      </div>
-    </div>
+    <a> made by @KOPIBARIK & @axel_skl </a>
+    <a></a>
   </div>
 </footer>
  
